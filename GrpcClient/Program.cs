2bef1c10b77d3e7af289522bd952b06ed37978ca
Alireza Ahmadi <ahmadialireza317@gmail.com>
2023-12-13 20:07:23 +0330

    init and run a grpc sample

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7254");
+var input = new HelloRequest { Name="Alireza",Age = 22,IsHim = true };
+var client = new Greeter.GreeterClient(channel);
+var answer = client.SayHello(input);
+Console.WriteLine(answer.Message);
+Console.ReadKey();
